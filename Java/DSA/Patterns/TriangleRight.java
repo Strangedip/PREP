acc781c2acff62182c5e77c0c84150792830dee7
Strangedip <sg02295@gmail.com>
2023-08-03 23:17:35 +0530

    right TriangleRight

--- Java/DSA/Patterns/TriangleRight.java
+++ Java/DSA/Patterns/TriangleRight.java
@@ -4,9 +4,15 @@ public class TriangleRight {
     public static void main(String[] args) {
         int n=5;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <i; j++) {
-                
+            int k=n-1-i;
+                while(k>0){
+                    System.out.print(" ");
+                    k--;
+                }
+            for (int j = 0; j <=i; j++) {
+                System.out.print("*");
             }
+            System.out.println();
         }
     }
 }
