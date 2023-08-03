f90748bec83e49ab9b1e4fdd11f5a32ad157e2de
Strangedip <sg02295@gmail.com>
2023-08-03 20:08:22 +0530

    asd

--- Java/DSA/Strings and Builder/StringBuilderClass.java
+++ Java/DSA/Strings and Builder/StringBuilderClass.java
@@ -4,11 +4,14 @@ public class StringBuilderClass {
     // used to concatenate objects(toStrings) without creating extra String object
     // it will modify string without creating new String object (less space consumed)
     public static void main(String[] args) {
+
+        // StringBuilder sb= new StringBuilder();
         StringBuilder sb= new StringBuilder("a");
+
         StringBuilderClass r=new StringBuilderClass();
         sb.append('b');
         sb.append("c");
-        sb.append(r); //appentds class toString value (address here)
+        sb.append(r); //appends class's toString value (address here)
         System.out.println(sb.append(false));
     }
 }
