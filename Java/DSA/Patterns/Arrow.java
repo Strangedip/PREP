2ff29b473183495f8aaf276873b040be598d21f6
Strangedip <sg02295@gmail.com>
2023-08-04 20:59:02 +0530

    arrow3

--- Java/DSA/Patterns/Arrow.java
+++ Java/DSA/Patterns/Arrow.java
@@ -2,47 +2,58 @@ package DSA.Patterns;
 
 public class Arrow {
     public static void main(String[] args) {
+        arrow(10);
         arrow1(10);
+        arrow3(5);
     }
 
     static void arrow(int n) {
-        for (int i = 1; i <= n/2; i++) {
+        for (int i = 1; i < n / 2; i++) {
             int j = 1;
             while (j <= i) {
-                System.out.print("*");
+                System.out.print("* ");
                 j++;
             }
             System.out.println();
         }
-        
-        for (int i = n/2; i >=0; i--) {
-            int j = i;
-            while (j >=0) {
-                System.out.print("*");
+
+        for (int i = n / 2; i >= 0; i--) {
+            int j = i-1;
+            while (j >= 0) {
+                System.out.print("* ");
                 j--;
             }
             System.out.println();
         }
     }
 
-    static void arrow1(int n){
+    static void arrow1(int n) {
         for (int i = 1; i < n; i++) {
-            if(i<(n/2)){
-                int j=0;
-                while(j<i){
-                    System.out.print("*");
+            if (i < (n / 2)) {
+                int j = 0;
+                while (j < i) {
+                    System.out.print("* ");
                     j++;
                 }
                 System.out.println();
-            }
-            else{
-                int j=n-i;
-                while(j>0){
-                    System.out.print("*");
+            } else {
+                int j = n - i;
+                while (j > 0) {
+                    System.out.print("* ");
                     j--;
                 }
                 System.out.println();
             }
+        }
+    }
+
+    static void arrow3(int n) {
+        for (int row = 0; row < 2 * n; row++) {
+            int col = (row > n ? 2 * n - row : row);
+            for (int j = 0; j < col; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
