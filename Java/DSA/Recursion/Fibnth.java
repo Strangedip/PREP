cb50127d0d87255c6272758bbf71fadee2813687
Strangedip <sg02295@gmail.com>
2023-08-05 13:39:11 +0530

    tail recursion

--- Java/DSA/Recursion/Fibnth.java
+++ Java/DSA/Recursion/Fibnth.java
@@ -4,13 +4,21 @@ public class Fibnth {
     public static void main(String[] args) {
         System.out.println(fibNth(6));
     }
-    static int fibNth(int n){
-        if(n==0){
+
+    static int fibNth(int n) {
+        if (n == 0) {
             return 0;
         }
-        if(n==1){
+        if (n == 1) {
             return 1;
         }
-        return fibNth(n-1)+fibNth(n-2);
+        // OR
+        // if(n<2){
+        // return n;
+        // }
+        return fibNth(n - 1) + fibNth(n - 2);   //not a tail recursion
+    }
+
+    static void fibPrint(int f, int s, int n) {
     }
 }
