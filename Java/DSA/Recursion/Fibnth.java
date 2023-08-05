d8adf1e42e62fe8f6e1b5832d8e49e8410939143
Strangedip <sg02295@gmail.com>
2023-08-05 13:29:31 +0530

    Fib nth num

--- Java/DSA/Recursion/Fibnth.java
+++ Java/DSA/Recursion/Fibnth.java
@@ -2,6 +2,15 @@ package DSA.Recursion;
 
 public class Fibnth {
     public static void main(String[] args) {
-        
+        System.out.println(fibNth(6));
+    }
+    static int fibNth(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fibNth(n-1)+fibNth(n-2);
     }
 }
