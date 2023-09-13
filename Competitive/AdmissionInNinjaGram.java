4b4aeb2de5dceef2e53d2d338b629c6021b13cb3
Strangedip <sg02295@gmail.com>
2023-09-13 23:18:51 +0530

    testCode

--- Competitive/AdmissionInNinjaGram.java
+++ Competitive/AdmissionInNinjaGram.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -20,23 +21,32 @@ public class AdmissionInNinjaGram {
         ans.add(5);
         ans.add(5);
         ans.add(5);
-        int k=2;
-        int[] a = { 2 ,5, 6, 2 };
-        int[] b = { 2 ,5, 1,3 };
-        System.out.println((sortedArray(a,b)));
+        int k = 2;
+        int[] a = { 2, 2, 1, 3, 1, 1, 3, 1, 1 };
+        int[] b = { 2, 5, 1, 3 };
+        System.out.println((sol(a)));
         // System.out.println(Arrays.toString(rotateArray(a, a.length)));
 
     }
+    public static void sort(int[]a,int low,int high){
+        int pivot=a[low];
+        sort(a, low, high);
+    }
 
-    public static List< Integer > sortedArray(int []a, int []b) {
-        // Write your code here
-        Set<Integer> s= new TreeSet<>();
+    public static int sol(int[] a) {
+        Map<Integer, Integer> mp = new HashMap<>();
         for (int i = 0; i < a.length; i++) {
-            s.add(a[i]);
+            if (!mp.containsKey(a[i])) {
+                mp.put(a[i], 1);
+            } else {
+                mp.put(a[i], mp.get(a[i]) + 1);
+            }
         }
-        for (int i = 0; i < b.length; i++) {
-            s.add(b[i]);
+        for (Map.Entry<Integer, Integer> m : mp.entrySet()) {
+            if (m.getValue() >= a.length / 2) {
+                return m.getKey();
+            }
         }
-        return new ArrayList<>(s);
+        return -1;
     }
 }
