787b38a2165c5ec8dd03a2c19e8bfe53b6071925
Strangedip <sg02295@gmail.com>
2023-09-21 02:31:51 +0530

    deleted quicksort approach

--- Java/DSA/sorting/QuickSort.java
+++ Java/DSA/sorting/QuickSort.java
@@ -11,22 +11,7 @@ public class QuickSort {
 
     public static void quickSort(int[] a, int low, int high) {
 
-        int piviot = a[low];
-        int left = low;
-        int right = high;
-        quickSort(a, low, right);
-        while (left <= right) {
-            if (a[left] <= piviot) {
-                left++;
-            } else if (a[left] > piviot) {
-                swap(a, left, right);
-            }
-            if (a[right] >= piviot) {
-                right--;
-            } else {
-                swap(a, left, right);
-            }
-        }
+       
     }
 
     public static void swap(int[] a, int i, int j) {
