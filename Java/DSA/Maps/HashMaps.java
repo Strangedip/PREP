e64e43d68b923c5aea824b27bc6ac75cefa96c62
Sandip Gupta <sg02295@gmail.com>
2025-06-21 17:31:55 +0530

    updated structure of the repository

--- Java/DSA/Maps/HashMaps.java
+++ Java/DSA/Maps/HashMaps.java
@@ -1,4 +1,4 @@
-package DSA.Hashing;
+package DSA.Maps;
 
 import java.util.*;
 
---
+++ Java/Experiment.java
@@ -0,0 +1,7 @@
+public class Experiment {
+    public static void main(String[] args) {
+        // This is a placeholder for the main method.
+        // You can add your code here to perform experiments or tests.
+        System.out.println("Experiment class is running.");
+    }
+}
